3c057d07b72eb3f097414d835aeb13023eed98f0
Hans Bala <hansbala@hansbala.com>
2018-02-15 11:48:56 +0530

    Update swap_words_inplace_char_array.java

--- swap_words_inplace_char_array.java
+++ swap_words_inplace_char_array.java
@@ -8,23 +8,29 @@ class swap_words_inplace_char_array {
   public static void main(String[] args) {
     int index_next = 4;
     char[] A = "hansapple".toCharArray();
+    // Reverse entire string to obtain swapped words in reversed order
     for (int i = 0; i <= (A.length - 1) / 2; ++i) {
       swap(A, i, A.length - i - 1);
     }
+    // Length of 1st and 2nd words to make it easier to understand
     int first_len = A.length - index_next, sec_len = index_next;
+    // Reverse the 1st word
     for (int i = 0; i <= (first_len - 1) / 2; ++i) {
       swap(A, i, first_len - i - 1);
     }
     int k = 1;
+    // Reverse the 2nd word
     for (int i = first_len; i <= (first_len + A.length - 1) / 2; ++i) {
       swap(A, i, A.length - k);
       ++k;
     }
+    // Finally print it
     for (int i = 0; i < A.length; i++) {
       System.out.print(A[i] + "");
     }
     System.out.println();
   }
+  // Swap algorithm
   public static void swap(char[] A, int i, int j) {
     char temp = A[i];
     A[i] = A[j];
